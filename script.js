e8b8e47bb8fa5931c05e18d3ec791a9d1d05abd3
yukihira-ali <ali_asnawi93@yahoo.com>
2024-08-15 18:58:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+function startGame() {
+  const guess = prompt('What does this 🥙 stand for?');
+
+  if (guess.toLowerCase() === 'makan tak habis simpan esok') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was makan tak habis simpan esok`);
+  }
+
+  const guess2 = prompt('What does this 😏 stand for?');
+
+  if (guess2.toLowerCase() === 'muka bangga walaupun jawab salah') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was muka bangga walaupun jawab salah`);
+  }
+
+  const guess3 = prompt('What does this 🫡 stand for?');
+
+  if (guess3.toLowerCase() === 'yessirrr') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was yessirrr`);
+  }
+
+  const guess4 = prompt('What does this 💔 stand for?');
+
+  if (guess4.toLowerCase() === 'crush suka orang lain') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was crush suka orang lain`);
+  }
+
+  const guess5 = prompt('What does this 🤨 stand for?');
+
+  if (guess5.toLowerCase() === 'betul ke serius?') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was betul ke serius?`);
+  }
+
+  const guess6 = prompt('What does this 🤕 stand for?');
+
+  if (guess6.toLowerCase() === 'jangan kelahi') {
+    alert('You guessed correctly!');
+  } else {
+    alert(`Sorry the correct answer was jangan kelahi`);
+  }
+}
